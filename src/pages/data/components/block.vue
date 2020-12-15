928757bb2ca60f8e13c378041eaa50c2bb01c6ec
zzzhim <3218566389@qq.com>
2020-12-15 14:52:38 +0800

    feat：订阅统计功能完成

---
+++ src/pages/data/components/block.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="colorBlock">
+        <slot>
+            <div class="title">{{ title }}</div>
+            <div class="num">{{ num }}</div>
+        </slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "colorBlock",
+        props: {
+            title: String,
+            num: [ String, Number ],
+        },
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    .colorBlock {
+        box-sizing: border-box;
+        width: 220px;
+        height: 140px;
+        background: #62C5F9;
+        border-radius: 5px;
+        text-align: center;
+
+        .title {
+            padding-top: 35px;
+            font-size: 18px;
+            font-family: PingFangSC-Regular, PingFang SC;
+            font-weight: 400;
+            color: #FFFFFF;
+            line-height: 25px;
+        }
+
+        .num {
+            padding-top: 10px;
+            font-size: 24px;
+            font-family: PingFangSC-Semibold, PingFang SC;
+            font-weight: 600;
+            color: #FFFFFF;
+            line-height: 33px;
+        }
+    }
+</style>

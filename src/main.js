f2bdcc75f37fcef61753dc453c94d0d4be3d8440
system.wangyafei <3218566389@qq.com>
2020-12-19 19:17:00 +0800

    删除无用方法

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import ElementUI from 'element-ui';
 import "@/styles/base.scss";
 import '@/styles/element-variables.scss';
 import 'element-ui/lib/theme-chalk/index.css';
-import ajax from './tools/ajax';
 import store from './store/index';
 Vue.use(ElementUI);
-Vue.use(ajax);
 
 Vue.config.productionTip = false;
 
